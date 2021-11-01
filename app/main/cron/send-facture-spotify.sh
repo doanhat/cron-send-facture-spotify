0fa65820137db835b2a6d1f4d778cd6f58d8c73e
DOAN Nhat Minh (Canal Plus) <nhatminh.doan@canal-plus.com>
2021-11-01 16:52:47 +0100

    Test

--- app/main/cron/send-facture-spotify.sh
+++ app/main/cron/send-facture-spotify.sh
@@ -8,11 +8,11 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   TOMORROW=`date -v+1d +%d`
 fi
 # See if tomorrow's day is some day
-if [ "$TOMORROW" -eq 01 ]; then
+if [ "$TOMORROW" -eq 02 ]; then
 	# shellcheck disable=SC2164
 	# shellcheck disable=SC2046
 	# TODO : export thread_in in environment variable type number
-	/usr/local/bin/docker run -e FB_USER_EMAIL_ADDRESS="$FB_USER_EMAIL_ADDRESS" -e FB_USER_PASSWORD="$FB_USER_PASSWORD" -e FB_THREAD_ID=6947468915279299 cron-send-facture-spotify:latest
+	/usr/local/bin/docker run -e FB_USER_EMAIL_ADDRESS="$FB_USER_EMAIL_ADDRESS" -e FB_USER_PASSWORD="$FB_USER_PASSWORD" -e FB_THREAD_ID=100003782897932 cron-send-facture-spotify:latest
 	exit
 else
   echo "Tomorrow is not 01"
