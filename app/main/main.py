b4bee03943095965104fae42279598bb5fa0f6fd
DOAN Nhat Minh (Canal Plus) <nhatminh.doan@canal-plus.com>
2021-12-05 17:39:26 +0100

    Update : change payment method to Paypal

--- app/main/main.py
+++ app/main/main.py
@@ -1,22 +1,31 @@
 import os
 from datetime import datetime, timedelta
 
+from flask import Flask, request
+
 from app.main.helper.logger import logger
-from app.main.task.get_gmail_content import get_first_mail_content, get_google_service
+from app.main.task.get_gmail_content import get_mail_contents, get_google_service
 from app.main.task.send_message_fb import send_msg_to_thread
-from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def send_receipt():
+    args = request.args.to_dict(flat=False)
+    logger.info(args)
     tomorrow_timestamp = datetime.today() - timedelta(hours=1) + timedelta(days=1)
     logger.debug(tomorrow_timestamp)
-    if tomorrow_timestamp.day == 1:
-        content = get_first_mail_content(get_google_service(), 'no-reply@spotify.com', 'reçu', ['TVA', 'Total', 'reçu'])
-        if content:
-            message = f"Facture Spotify : {content} - Message automatique"
+    if tomorrow_timestamp.day == 6:
+        data = \
+            get_mail_contents(get_google_service(), args.get('sender'), args.get('subject'), args.get('key_words'))
+
+        if data:
+            data = data[0]
+            message = f"Message automatique : " \
+                      f"{data.subject} à {data.received_date} : " \
+                      f"{data.sender} : " \
+                      f"{data.content}"
             send_msg_to_thread(message)
             return message
         else:
